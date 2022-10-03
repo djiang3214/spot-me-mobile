2c4d0d4c1d940be2b4914bf2a254b5734df7f0f7
Derek Jiang <djiang3214@gmail.com>
2022-10-03 00:42:08 -0400

    Made a general layout for the Home/Login page

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const backgroundImage = {uri: 'https://wallpaperaccess.com/full/2581402.jpg'};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.image}>
+      <Text style={styles.titleText}>Spot Me</Text>
+      <Text style={styles.bodyText}>Welcome!</Text>
+      <StatusBar style="auto"/>
+    <View style={styles.fixToText}>
+        <Button
+          title="Login"
+          onPress={() => Alert.alert('Login button')}
+          color="#808080"
+        />
+        <Button
+          title="Sign Up"
+          onPress={() => Alert.alert('Sign up button')}
+          color="#808080"
+        />
     </View>
+    </ImageBackground>
+    </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
+  titleText:{
+    fontSize: 40,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  bodyText: {
+    textAlign: 'center',
+    fontSize: 35,
+    color: 'orange',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
